5185bc73c9aee1be8f324e5a93e1e49e285f4d71
DraconicArcher <136211383+DraconicArcher@users.noreply.github.com>
2025-02-01 01:58:25 -0500

    Initial commit

---
+++ src/main/java/com/draconicarcher/brewincompatdelight/items/BCDFood.java
@@ -0,0 +1,23 @@
+package com.draconicarcher.brewincompatdelight.items;
+
+import com.draconicarcher.brewincompatdelight.Brewincompatdelight;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import umpaz.brewinandchewin.common.registry.BnCEffects;
+
+
+
+public class BCDFood {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Brewincompatdelight.MODID);
+
+    public static final FoodProperties MOONSHINE = (new FoodProperties.Builder()).effect(() -> {
+        return new MobEffectInstance((MobEffect) BnCEffects.TIPSY.get(), 3600, 1);
+    }, 1.0F).effect(() -> {
+        return new MobEffectInstance((MobEffect) BnCEffects.INTOXICATION.get(), 3000, 0);
+    }, 1.0F).alwaysEat().build();
+}
